e778bae314ba27c7c7ec3247832565d3454de6ed
jannik <118477717+Jk-Ow@users.noreply.github.com>
2023-04-15 11:11:52 +0200

    Edited movement-script for topdown gameplay

--- GodotProjekt/Code/player_character.gd
+++ GodotProjekt/Code/player_character.gd
@@ -1,28 +1,36 @@
 extends CharacterBody2D
 
+@export var move_speed : float = 100
+@export var running_speed : float = 200
+@export var starting_direction : Vector2 = Vector2(0 , 1)
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@onready var animation_tree = $AnimationTree
+@onready var state_machine = animation_tree.get("parameters/playback")
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+func _ready():
+	update_animation_parameters(starting_direction)
 
-
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+func _physics_process(_delta):
+	var input_direction = Vector2(
+		Input.get_action_strength("right") - Input.get_action_strength("left"),
+		Input.get_action_strength("down") - Input.get_action_strength("up")
+	)
+	input_direction = input_direction.normalized()
+	update_animation_parameters(input_direction)
+	pick_new_state()
+	if(Input.get_action_strength("run") > 0):
+		velocity = input_direction * running_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity = input_direction * move_speed
+	move_and_slide()#
+	
+func update_animation_parameters(move_input : Vector2):
+	if(move_input != Vector2.ZERO):
+		animation_tree.set("parameters/Walk/blend_position", move_input)
+		animation_tree.set("parameters/Idle/blend_position", move_input)
 
-	move_and_slide()
+func pick_new_state():
+	if(velocity != Vector2.ZERO):
+		state_machine.travel("Walk")
+	else:
+		state_machine.travel("Idle")
