048cea0dcc74d17f166a5c1156e1dc01b8dbcb9f
jannik <118477717+Jk-Ow@users.noreply.github.com>
2023-04-17 10:26:27 +0200

    Removed Animation logic from CharacterScript

--- GodotProjekt/Code/player_character.gd
+++ GodotProjekt/Code/player_character.gd
@@ -2,13 +2,7 @@ extends CharacterBody2D
 
 @export var move_speed : float = 100
 @export var running_speed : float = 200
-@export var starting_direction : Vector2 = Vector2(0 , 1)
 
-@onready var animation_tree = $AnimationTree
-@onready var state_machine = animation_tree.get("parameters/playback")
-
-func _ready():
-	update_animation_parameters(starting_direction)
 
 func _physics_process(_delta):
 	var input_direction = Vector2(
@@ -16,21 +10,9 @@ func _physics_process(_delta):
 		Input.get_action_strength("down") - Input.get_action_strength("up")
 	)
 	input_direction = input_direction.normalized()
-	update_animation_parameters(input_direction)
-	pick_new_state()
+	
 	if(Input.get_action_strength("run") > 0):
 		velocity = input_direction * running_speed
 	else:
 		velocity = input_direction * move_speed
-	move_and_slide()#
-	
-func update_animation_parameters(move_input : Vector2):
-	if(move_input != Vector2.ZERO):
-		animation_tree.set("parameters/Walk/blend_position", move_input)
-		animation_tree.set("parameters/Idle/blend_position", move_input)
-
-func pick_new_state():
-	if(velocity != Vector2.ZERO):
-		state_machine.travel("Walk")
-	else:
-		state_machine.travel("Idle")
+	move_and_slide()
