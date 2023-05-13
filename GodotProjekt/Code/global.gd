e9723bf305ad659be30169b7420c375733f00621
MKON22 <mar.kurewitz.22@lehre.mosbach.dhbw.de>
2023-05-13 12:21:44 +0200

    Added Hintergrundmusik without repeat

--- GodotProjekt/Code/global.gd
+++ GodotProjekt/Code/global.gd
@@ -4,3 +4,8 @@ var player_attacking = true
 
 @onready var player = get_node("/root/startLevel/playerCharacter")
 var rng = RandomNumberGenerator.new()
+
+func audio():
+	if $AudioStreamPlayer.playing == false:
+		$AudioStreamPlayer.play()
+pass
