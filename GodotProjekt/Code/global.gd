52bb4730180143f4fc6f10446887a551a79c1c84
MKON22 <mar.kurewitz.22@lehre.mosbach.dhbw.de>
2023-05-13 21:17:32 +0200

    corrected audio function

--- GodotProjekt/Code/global.gd
+++ GodotProjekt/Code/global.gd
@@ -5,6 +5,6 @@ var player_attacking = true
 @onready var player = get_node("/root/startLevel/playerCharacter")
 var rng = RandomNumberGenerator.new()
 
-func _ready():
+func _audio():
 	if ($AudioStreamPlayer.playing == false):
 		$AudioStreamPlayer.play()
