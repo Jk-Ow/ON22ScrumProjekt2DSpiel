525ee65d4471c81461ca6749e58be6b066d1626f
jannik <118477717+Jk-Ow@users.noreply.github.com>
2023-05-14 14:20:30 +0200

    added update health function

--- GodotProjekt/Code/hud_standard.gd
+++ GodotProjekt/Code/hud_standard.gd
@@ -6,6 +6,8 @@ func _ready():
 	
 	if(!global.player.update_health_plants.is_connected(_on_player_update_health_plants)):
 		global.player.update_health_plants.connect(_on_player_update_health_plants)
+	if(!global.player.update_health.is_connected(_on_player_update_health)):
+		global.player.update_health.connect(_on_player_update_health)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,3 +16,6 @@ func _process(_delta):
 
 func _on_player_update_health_plants(amount):
 	$health_plants/health_plant_amount.text = str(amount)
+
+func _on_player_update_health(remaining_health):
+	$current_health.text = "Health: " + str(remaining_health)
