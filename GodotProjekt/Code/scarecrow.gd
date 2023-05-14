e4cfd5ad087c60dcf23804c0e469f3c663180301
jannik <118477717+Jk-Ow@users.noreply.github.com>
2023-05-14 11:35:28 +0200

    scarecrow now hit only enemy

--- GodotProjekt/Code/scarecrow.gd
+++ GodotProjekt/Code/scarecrow.gd
@@ -1,24 +1,11 @@
 extends StaticBody2D
 var player_in_range = false
-var aware_of_player = false
-var cooldown = true
-var health = 100
-signal mob_attacking
 
 func _physics_process(_delta):
-	attack_player()
-	if(health <= 0):
-		self.queue_free()
-
-func attack_player():
-	if(cooldown and aware_of_player):
-		mob_attacking.emit()
-		cooldown = false
-		$attack_cooldown.start()
+	pass
 
 func _on_player_character_attacking():
 	if(player_in_range):
-		health -= 15
 		$AnimationPlayer.play("hurt")
 
 func _on_area_2d_body_entered(body):
@@ -29,13 +16,3 @@ func _on_area_2d_body_entered(body):
 func _on_area_2d_body_exited(body):
 	if(body.has_method("player")):
 		player_in_range = false
-		aware_of_player = false
-
-func _on_attack_cooldown_timeout():
-	cooldown = true
-	$attack_cooldown.stop()
-
-
-func _on_reaction_delay_timeout():
-	aware_of_player = true
-	$reaction_delay.stop()
