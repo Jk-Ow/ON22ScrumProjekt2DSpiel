3b04c473db49937dcdd32cf5eecd627a8fbc4ec9
jannik <118477717+Jk-Ow@users.noreply.github.com>
2023-05-14 14:04:49 +0200

    added figthing script and necesary nodes to crab

---
+++ GodotProjekt/Code/crab.gd
@@ -0,0 +1,78 @@
+extends CharacterBody2D
+var player_in_range = false
+var aware_of_player = false
+var cooldown = true
+var health = 100
+var speed = 150
+var direction
+var last_direction
+signal mob_attacking(damage)
+
+func _ready():
+	var attention_zone = get_node("AttentionZone")
+	var attack_zone = get_node("AttackZone")
+	var attack_cooldown = get_node("attack_cooldown")
+	if(!global.player.attacking.is_connected(_on_player_character_attacking)):
+		global.player.attacking.connect(_on_player_character_attacking)
+	if(!attention_zone.body_entered.is_connected(_on_attention_zone_body_entered)):
+		attention_zone.body_entered.connect(_on_attention_zone_body_entered)
+	if(!attention_zone.body_exited.is_connected(_on_attention_zone_body_exited)):
+		attention_zone.body_exited.connect(_on_attention_zone_body_exited)
+	if(!attack_zone.body_entered.is_connected(_on_attack_zone_body_entered)):
+		attack_zone.body_entered.connect(_on_attack_zone_body_entered)
+	if(!attack_zone.body_exited.is_connected(_on_attack_zone_body_exited)):
+		attack_zone.body_exited.connect(_on_attack_zone_body_exited)
+	if(!attack_cooldown.timeout.is_connected(_on_attack_cooldown_timeout)):
+		attack_cooldown.timeout.connect(_on_attack_cooldown_timeout)
+
+func _process(_delta):
+	if(health > 0):
+		move_to_player()
+		attack_player()
+		if(health <= 0):
+			self.queue_free()
+
+func move_to_player():
+	if(aware_of_player):
+		direction = (global.player.get_global_position() - self.get_global_position())
+		direction = direction.normalized()
+		if(direction != Vector2.ZERO):
+			last_direction = direction
+		velocity = direction * speed
+		move_and_slide()
+func attack_player():
+	if(cooldown and player_in_range):
+		mob_attacking.emit(10)
+		cooldown = false
+		$attack_cooldown.start()
+
+func _on_player_character_attacking():
+	if(player_in_range):
+		health -= 15
+		$AnimationPlayer.play("hurt")
+
+func _on_attack_cooldown_timeout():
+	cooldown = true
+
+
+func _on_attack_zone_body_entered(body):
+	if(body.has_method("player")):
+		player_in_range = true
+		#$AnimationTree.set()
+
+
+func _on_attack_zone_body_exited(body):
+	if(body.has_method("player")):
+		player_in_range = false
+
+
+
+func _on_attention_zone_body_entered(body):
+	if(body.has_method("player")):
+		aware_of_player = true
+		#$AnimationTree.set("parameters/playback",a)
+
+
+func _on_attention_zone_body_exited(body):
+	if(body.has_method("player")):
+		aware_of_player = false
