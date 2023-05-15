1ba039532a93f1eaf6cbe4fc5dc6dc90def55d49
jannik <118477717+Jk-Ow@users.noreply.github.com>
2023-05-15 17:44:02 +0200

    added playerdeath to fight scene

--- GodotProjekt/Code/FuchsRB.gd
+++ GodotProjekt/Code/FuchsRB.gd
@@ -2,22 +2,29 @@ extends StaticBody2D
 
 signal attack(damage)
 
-var player_alive = true
-
 @onready var stateMachine = $AnimationTree.get("parameters/playback")
+
+func _ready():
+	if(!$AnimationTree.animation_finished.is_connected(_on_animation_tree_animation_finished)):
+		$AnimationTree.animation_finished.connect(_on_animation_tree_animation_finished)
 
 func _physics_process(_delta):
 	$HealthBarFox.value = global.health
-	if (Input.is_action_just_pressed("attack") and player_alive):
+	if (Input.is_action_just_pressed("attack") and global.health > 0):
 		stateMachine.travel("Attack")
 		attack.emit(15)
-	if (Input.is_action_just_pressed("heal") and player_alive and global.health_plant_amount>0):
+	if (Input.is_action_just_pressed("heal") and global.health > 0 and global.health_plant_amount>0):
 		global.health += 30
 		global.health_plant_amount -= 1
 
 func _on_baum_mob_attacking(damage):
 	global.health -= damage
-	stateMachine.travel("Hurt")
-	if (global.health <= 0 and player_alive) :
-		player_alive = false
+	if(global.health > 0):
+		stateMachine.travel("Hurt")
+	else:
 		stateMachine.travel("Death")
+
+
+func _on_animation_tree_animation_finished(anim_name):
+	if(anim_name == "Death"):
+		global.start_new_game = true
