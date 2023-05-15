f30288f56baa12a77be4ede1cbb1945db433106c
jannik <118477717+Jk-Ow@users.noreply.github.com>
2023-05-15 13:03:30 +0200

    added Healthbar to hud

--- GodotProjekt/Code/hud_standard.gd
+++ GodotProjekt/Code/hud_standard.gd
@@ -16,4 +16,8 @@ func _on_player_update_health_plants(amount):
 	$health_plants/health_plant_amount.text = str(amount)
 
 func _on_player_update_health(remaining_health):
-	$current_health.text = "Health: " + str(remaining_health)
+	$current_health.text = str(remaining_health)
+	if(remaining_health <= 0):
+		$PanelContainer/Sprite2D.set_frame(10)
+	else:
+		$PanelContainer/Sprite2D.set_frame(10 - floor(remaining_health/10))
